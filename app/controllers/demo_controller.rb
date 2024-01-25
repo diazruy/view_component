506b0c691cbc9f80c5b147843fba842c99d58f8d
Ruy Diaz <diazruy@gmail.com>
2024-01-25 09:38:28 -0800

    Different column count

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class DemoController < ApplicationController
+  PAGE_SIZE = 16
+
   def index
     books = Book.all
     if (sort_by = params[:books_sort_by]).present?
@@ -10,7 +12,7 @@ class DemoController < ApplicationController
     if (sort_by = params[:characters_sort_by]).present?
       characters = characters.order(sort_by)
     end
-    @pagy_books, @books = pagy(books, page_param: :books_page)
-    @pagy_characters, @characters = pagy(characters, page_param: :characters_page)
+    @pagy_books, @books = pagy(books, page_param: :books_page, items: PAGE_SIZE)
+    @pagy_characters, @characters = pagy(characters, page_param: :characters_page, items: PAGE_SIZE)
   end
 end
