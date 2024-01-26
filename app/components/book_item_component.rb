17ea4d70c4eb1abe3cc0d3d3e09d01445dc180da
Ruy Diaz <diazruy@gmail.com>
2024-01-26 09:37:04 -0800

    Add BookItemComponent

---
+++ app/components/book_item_component.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class BookItemComponent < ApplicationComponent
+  attr_reader :book
+
+  def initialize(book)
+    @book = book
+  end
+
+  def icon
+    case book.genre
+    when /Science fiction/ then :alien
+    when /Fantasy/ then :dragon
+    when /Mystery/ then :eye
+    when /Romance/ then :heart
+    when /Thriller/ then :skull
+    when /Biography/ then :user
+    when /Legend/ then :scroll
+    when /Fairy/ then :'wand-sparkles'
+    when /Speech/ then :comment
+    else :book
+    end
+  end
+
+  def full_name
+    first_name, last_name = book.author.split(' ')
+    "#{last_name}, #{first_name}"
+  end
+end
