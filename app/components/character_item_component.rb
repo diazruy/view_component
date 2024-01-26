989c67d61b333685c43b8879c5e5ee8b0842052b
Ruy Diaz <diazruy@gmail.com>
2024-01-26 09:57:59 -0800

    Refactor CharacterItemComponent

--- app/components/character_item_component.rb
+++ app/components/character_item_component.rb
@@ -6,4 +6,19 @@ class CharacterItemComponent < ApplicationComponent
   def initialize(character)
     @character = character
   end
+
+  def abilities
+    [
+      { ability: 'Strength', score: character.strength, icon: 'dumbbell' },
+      { ability: 'Dexterity', score: character.dexterity, icon: 'feather-pointed' },
+      { ability: 'Constitution', score: character.constitution, icon: 'shield-halved' },
+      { ability: 'Intelligence', score: character.intelligence, icon: 'brain' },
+      { ability: 'Wisdom', score: character.wisdom, icon: 'book-journal-whills' },
+      { ability: 'Charisma', score: character.charisma, icon: 'user-group', ability_initial: 'K' }
+    ]
+  end
+
+  def full_name
+    "#{character.last_name}, #{character.first_name}"
+  end
 end
