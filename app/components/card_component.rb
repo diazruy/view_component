b9892bc44bd180763ef8b4d342e6bcdfcfea5b05
Ruy Diaz <diazruy@gmail.com>
2024-01-26 10:11:53 -0800

    Add header slot to CardComponent

--- app/components/card_component.rb
+++ app/components/card_component.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class CardComponent < ApplicationComponent
+  renders_one :header
+
   attr_reader :title
 
-  def initialize(title:)
+  def initialize(title: nil)
     @title = title
     super()
   end
